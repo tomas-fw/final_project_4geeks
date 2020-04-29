a03e59608622422484a36292a889aaafcba2e255
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-04-29 12:43:47 -0400

    health team se comento en navBar y solo se usa boton, vista de chat de nutri y PT

--- src/components/healthteam.jsx
+++ src/components/healthteam.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Context } from '../store/appContext';
 import { Link } from 'react-router-dom';
+/* RESPONSIVE LISTO*/
 
 const HealthTeam = props => {
     const { store, actions } = useContext(Context)
@@ -8,45 +9,52 @@ const HealthTeam = props => {
     /* linea 26 llama al ultimo nutri*/
     return (
         <>
-            {
-                store.currentUser === null ?
-                    <div class="spinner-border" role="status">
-                        <span class="sr-only">Loading...</span>
-                    </div>
-                    :
-                    store.currentUser.user.planes_id.length == 0 ?
-                        <div class="alert alert-warning" role="alert">
-                            {store.error}
-                                    You have no active plans yet :(
-                                </div>
-                        :
+            <div className="container">
 
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-6 card">
-                                    <img src="..." className="card-img-top" alt="..." />
-                                    <div className="card-body">
-                                        <h5 className="card-title">{store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.nutritionist_name}</h5>
-                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <Link to={"/profile/professional/clients/contact/nutritionist/" +
-                                            store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.client_id + '/'+
-                                            store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.id} className="btn btn-primary">Contactar a mi nutricionista</Link>
-                                    </div>
-                                </div>
-                                <div className="col-md-6 card">
-                                    <img src="..." className="card-img-top" alt="..." />
-                                    <div className="card-body">
-                                        <h5 className="card-title">{store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.trainer_name}</h5>
-                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <Link to={"/profile/professional/clients/contact/trainer/" +
-                                            store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.client_id +'/'+
-                                            store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.id} className="btn btn-primary">Contactar a mi entrenador</Link>
-                                    </div>
-                                </div>
+                <h1 className="text-center mt-3 mb-3">Mi equipo de profesionales </h1>
 
-                            </div>
+                {
+                    store.currentUser === null ?
+                        <div class="spinner-border" role="status">
+                            <span class="sr-only">Loading...</span>
                         </div>
-            }
+                        :
+                        store.currentUser.user.planes_id.length == 0 ?
+                            <div class="alert alert-warning" role="alert">
+                                {store.error}
+                                    No tienes ningun plan contratado :(
+                                </div>
+                            :
+
+                            <div className="container">
+                                <div className="row">
+
+                                    <div className="col-md-5 col-xs-5 card border-success mt-4 ml-3">
+                                        <img src={store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.nutritionist_avatar} className="card-img-top" alt="..." width="150" height="150" />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.nutritionist_name} {store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.nutritionist_last_name} </h5>
+                                            <p className="card-text">Hola! Soy tu Nutricionista, si tienes alguna duda y necesitas contactarme, haz click en el siguiente botón </p>
+                                            <Link to={"/profile/professional/clients/contact/nutritionist/" +
+                                                store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.client_id + '/' +
+                                                store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.id} className="btn btn-success">Contactar a mi nutricionista</Link>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-1 col-xs-1"></div>
+                                    <div className="col-md-5 col-xs-5 card border-warning mt-4 ml-3">
+                                        <img src={store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.trainer_avatar} className="card-img-top" alt="..." width="150" height="150" />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.trainer_name} {store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.trainer_last_name}</h5>
+                                            <p className="card-text">Hola! Soy tu Personal Trainer, si tienes alguna duda y necesitas contactarme, haz click en el siguiente botón</p>
+                                            <Link to={"/profile/professional/clients/contact/trainer/" +
+                                                store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.client_id + '/' +
+                                                store.currentUser.user.planes_id[store.currentUser.user.planes_id.length - 1].all_plans.id} className="btn btn-warning">Contactar a mi entrenador</Link>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                }
+            </div>
 
         </>
     )
