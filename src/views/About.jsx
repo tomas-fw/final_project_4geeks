7c6371dc1246600d856a33db66173ad4fe23af59
Daniela Fuenmayor <danifuenmayor7@gmail.com>
2020-04-29 18:02:53 -0400

    completed components

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -6,27 +6,25 @@ const About = props => {
     return (
         <>
             <div className="container">
-                <div className="card mb-3">
+                <div className="card my-3 homeCard bg-transparent">
                     <div className="row no-gutters">
                         <div className="col-md-4">
                             <img src={historyImage} className="card-img team-cards" alt="..." />
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
-                                <h5 className="card-title">Our history</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                <h2 className="card-title font">Nuestra historia</h2>
+                                <p className="card-text font2">Somos un grupo de personas con diversos backgrounds relacionados a la salud, nos conocimos aprendiendo a programar y vimos una oportunidad de unir nuestros conocimientos para ofrecer un servicio especializado que podría hacer una diferencia en la vida de las personas. </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="card mb-3">
+                <div className="card mb-3 homeCard bg-transparent">
                     <div className="row no-gutters">
                         <div className="col-md-8">
                             <div className="card-body">
-                                <h5 className="card-title">Our mission</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                <h2 className="card-title font">Nuestra misión</h2>
+                                <p className="card-text font2">Que todos puedan vivir en las mejores condiciones! Creemos que todas las personas podrían aumentar su cálidad de vida en cuestión de un mes con el asesoramiento correcto y que esto debería ser fácil y accesible. Queremos que algo tan prioritario como un plan de salud personalizado.</p>
                             </div>
                         </div>
                         <div className="col-md-4">
